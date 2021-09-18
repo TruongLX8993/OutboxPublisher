cab04c7914dbbeb82514f6cbc6c80f779181ca2d
truonglx <truonglx8993@gmail.com>
2021-09-18 15:22:50 +0700

    update

--- Publisher.Oracle/OracleOutboxEventHandler.cs
+++ Publisher.Oracle/OracleOutboxEventHandler.cs
@@ -1,4 +1,8 @@
+using System;
 using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Threading;
 using Oracle.ManagedDataAccess.Client;
 
 namespace Publisher
@@ -6,35 +10,72 @@ namespace Publisher
     public class OracleOutboxEventHandler
 
     {
-    private readonly string _connectionString;
+        private readonly string _connectionString;
+        private OracleConnection _connection;
+        private bool _flag;
 
-    public OracleOutboxEventHandler(string connectionString)
-    {
-        _connectionString = connectionString;
-    }
+        public OracleOutboxEventHandler(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
 
-    public void Start()
-    {
-        var con = new OracleConnection(_connectionString);
+        public void Start()
+        {
+            _connection = (OracleConnection)OracleClientFactory.Instance.CreateConnection();
+            _connection.ConnectionString = _connectionString;
+            _connection.Open();
 
-        var cmd = con.CreateCommand();
-        cmd.CommandText = "select * from OUTBOX_EVENT";
+            var cmd = (OracleCommand)_connection.CreateCommand();
+            cmd.CommandText = "select * from OUTBOX_EVENT";
+            cmd.AddRowid = true;
+            // cmd.Notification.IsNotifiedOnce = false;
+            OracleDependency.Port = 1005; // default port.
+            var depend = new OracleDependency(cmd);
+            depend.OnChange += Handle;
+            cmd.ExecuteNonQuery();
 
-        OracleDependency.Port = 1005; // default port.
-        var depend = new OracleDependency(cmd);
-        depend.OnChange += new OnChangeEventHandler(Handle);
-    
-    }
+            // var tran = _connection.BeginTransaction();
+            // var cmdUpdate = _connection.CreateCommand();
+            // cmdUpdate.Transaction = tran;
+            // cmdUpdate.CommandText = @"update OUTBOX_EVENT
+            // set STATUS = STATUS + 1
+            // where id = '48725bad-a25e-4ea2-961a-84f9d82fb422'";
+            // cmd.ExecuteNonQuery();
+            // cmdUpdate.ExecuteNonQuery();
+            // tran.Commit();
+            while (!_flag)
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("nothing");
+            }
+        }
 
-    private void Handle(object sender, OracleNotificationEventArgs eventArgs)
-    {
+        private void Handle(object sender, OracleNotificationEventArgs args)
+        {
+            _flag = true;
+            DataRow detailRow = args.Details.Rows[0];
+            string rowid = detailRow["Rowid"]
+                .ToString();
+            // Console.WriteLine(rowid);
+            // for (int i = 1; i < args.Details.Rows.Count; i++)
+            // {
+            //     detailRow = args.Details.Rows[i];
+            //     rowid = detailRow["Rowid"]
+            //         .ToString();
+            //     Console.WriteLine(rowid);
+            // }
+            var cmdTxt = @$"update OUTBOX_EVENT
+            set STATUS = STATUS + 1
+            where ROWID = '{rowid}'";
+            var trans = _connection.BeginTransaction();
+            var cmd = new OracleCommand(cmdTxt, _connection);
+            
+        }
 
-    }
 
-    private void Listen()
-    {
-
-    }
-
+        public void Stop()
+        {
+            _connection?.Close();
+        }
     }
 }
--- Publisher.Test/UnitTest1.cs
+++ Publisher.Test/UnitTest1.cs
@@ -1,10 +1,12 @@
+using System.Threading;
 using NUnit.Framework;
 
 namespace Publisher.Test
 {
     public class Tests
     {
-        private const string ConnectionString = "";
+        private const string ConnectionString =
+            "Data Source=(DESCRIPTION =(ADDRESS=(PROTOCOL=TCP)(HOST=192.168.1.15)(PORT=1521))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=ONEMES3BC_NEW)));Password=O1234#ONEMES3BC_NEW;User ID=ONEMES3BC_NEW;Connection Lifetime=0;Connection Timeout=30;Max Pool Size=100;Incr Pool Size=5;Decr Pool Size=1;";
 
         private OracleOutboxEventHandler _handler;
 
@@ -18,7 +20,8 @@ namespace Publisher.Test
         public void Test1()
         {
             _handler.Start();
-            while (true) ;
+            Thread.Sleep(5000);
+            _handler.Stop();
         }
     }
 }
